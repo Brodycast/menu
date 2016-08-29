a6ba18e3b6b00a0958c678076b09bab3bafe51ed
Henri Cavalcante <hnri_mxel@hotmail.com>
2016-08-29 14:31:25 -0300

    ...

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
   firebase: {
-    apiKey: "AIzaSyAVHOYZaIOBiSbMoF7UwN02GgCYpw6jbrs",
-    authDomain: "callabria-bfe3f.firebaseapp.com",
-    databaseURL: "https://callabria-bfe3f.firebaseio.com",
-    storageBucket: "callabria-bfe3f.appspot.com",
+    apiKey: 'AIzaSyAVHOYZaIOBiSbMoF7UwN02GgCYpw6jbrs',
+    authDomain: 'callabria-bfe3f.firebaseapp.com',
+    databaseURL: 'https://callabria-bfe3f.firebaseio.com',
+    storageBucket: 'callabria-bfe3f.appspot.com'
   },
   logo: require('./images/logo.png'),
   placeName: 'Pizzaria Callabria'
